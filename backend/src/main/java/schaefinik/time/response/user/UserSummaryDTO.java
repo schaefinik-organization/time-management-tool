08bff16fdcc12c5286e4f9d8e374062c1a5b9a5d
Dominik Schäfer <schaefinik@gmail.com>
2026-08-13 21:49:59 +0700

    refactor: rename creationDate to createdAt in DTOs for consistency; remove unused constructor in UserSummaryDTO

--- backend/src/main/java/schaefinik/time/response/user/UserSummaryDTO.java
+++ backend/src/main/java/schaefinik/time/response/user/UserSummaryDTO.java
@@ -2,7 +2,6 @@ package schaefinik.time.response.user;
 
 import lombok.Getter;
 import lombok.Setter;
-import schaefinik.time.model.TimeUserModel;
 
 @Getter
 @Setter
@@ -10,12 +9,4 @@ public class UserSummaryDTO {
 	private Long id;
 	private String username;
 	private String role;
-
-	public UserSummaryDTO(TimeUserModel model) {
-		if (model != null) {
-			this.id = model.getId();
-			this.username = model.getUsername();
-			this.role = model.getRole().name();
-		}
-	}
 }
